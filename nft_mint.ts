443ee2b13041b74eb7ef707980e1ca9e7f9d85ef
Emre OZENSES <emreozenses@gmail.com>
2024-07-10 17:15:38 +0300

    completed

--- nft_mint.ts
+++ nft_mint.ts
@@ -7,8 +7,11 @@ import {
   publicKey,
   signerIdentity,
 } from '@metaplex-foundation/umi'
-import { createNft, mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata'
-import bs58 from "bs58"
+import {
+  createNft,
+  mplTokenMetadata,
+} from '@metaplex-foundation/mpl-token-metadata'
+import bs58 from 'bs58'
 
 const umi = createUmi('https://api.devnet.solana.com')
 const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet))
@@ -16,24 +19,23 @@ const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet))
 const signer = createSignerFromKeypair(umi, keypair)
 umi.use(signerIdentity(signer))
 
-umi.use(mplTokenMetadata());
+umi.use(mplTokenMetadata())
 
-const mint = generateSigner(umi);
+const mint = generateSigner(umi)
 
-(async()=>{
-    const url =
-      'https://arweave.net/bVNGS58RNkvt_IkQ0WHuEm2Ce_DmMv_hA-5BW2w7Vmw';
+;(async () => {
+  const url = 'https://arweave.net/bVNGS58RNkvt_IkQ0WHuEm2Ce_DmMv_hA-5BW2w7Vmw'
 
-    const tx = createNft(umi,{
-        mint,
-        name:'moda evim',
-        symbol:'ME',
-        uri: url,
-        sellerFeeBasisPoints: percentAmount(2)
-    });
+  const tx = createNft(umi, {
+    mint,
+    name: 'moda evim',
+    symbol: 'ME',
+    uri: url,
+    sellerFeeBasisPoints: percentAmount(2),
+  })
 
-    const res = await tx.sendAndConfirm(umi);
+  const res = await tx.sendAndConfirm(umi)
 
-    console.log(res.signature);
-    console.log(bs58.encode(res.signature));
-})();
+  console.log(res.signature)
+  console.log(bs58.encode(res.signature))
+})()
